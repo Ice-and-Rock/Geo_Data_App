b4f7abc39d825db357ec91a9c519a276c2938d84
nick v <nvalente.electrics@gmail.com>
2023-08-18 10:15:45 +0100

    created Typescript app and build server js

---
+++ renewable-energy-dashboard/server/server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/data', (req, res) => {
+  const mockData = [
+    { timestamp: '2023-08-18T12:00:00Z', windSpeed: 10, temperature: 25 },
+    { timestamp: '2023-08-18T13:00:00Z', windSpeed: 12, temperature: 26 },
+    // Add more data entries as needed
+  ];
+  res.json(mockData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
